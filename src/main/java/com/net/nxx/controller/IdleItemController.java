1dc472c6719648d1642700c437a58843c7ac85fc
191850189 <191850189@smail.nju.edu.cn>
2021-05-09 15:10:21 +0800

    添加订单查询、闲置管理功能

---
+++ src/main/java/com/net/nxx/controller/IdleItemController.java
@@ -0,0 +1,106 @@
+package com.net.nxx.controller;
+
+import com.net.nxx.common.exception.ErrorMsg;
+import com.net.nxx.model.NxxIdleItem;
+import com.net.nxx.service.IdleItemService;
+import com.net.nxx.vo.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.annotation.Resource;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author wt
+ * @create 2021-05-09
+ */
+@Api(tags = "IdleItemController", description = "闲置商品管理")
+@RestController
+@RequestMapping("idle")
+public class
+IdleItemController {
+
+    @Autowired
+    private IdleItemService idleItemService;
+
+    @ApiOperation("发布闲置")
+    @PostMapping("add")
+    public ResultVo addIdleItem(@CookieValue("shUserId")
+                                    @NotNull(message = "登录异常 请重新登录")
+                                    @NotEmpty(message = "登录异常 请重新登录") String shUserId,
+                                @RequestBody NxxIdleItem nxxIdleItem){
+        nxxIdleItem.setUserId(Long.valueOf(shUserId));
+        nxxIdleItem.setIdleStatus((byte) 1);
+        nxxIdleItem.setReleaseTime(new Date());
+        if(idleItemService.addIdleItem(nxxIdleItem)){
+            return ResultVo.success(nxxIdleItem);
+        }
+        return ResultVo.fail(ErrorMsg.SYSTEM_ERROR);
+    }
+
+    @ApiOperation("查询闲置信息，同时查出发布者的信息")
+    @GetMapping("info")
+    public ResultVo getIdleItem(@RequestParam Long id){
+        return ResultVo.success(idleItemService.getIdleItem(id));
+    }
+
+    @ApiOperation("查询用户发布的所有闲置")
+    @GetMapping("all")
+    public ResultVo getAllIdleItem(@CookieValue("shUserId")
+                                       @NotNull(message = "登录异常 请重新登录")
+                                       @NotEmpty(message = "登录异常 请重新登录") String shUserId){
+        return ResultVo.success(idleItemService.getAllIdelItem(Long.valueOf(shUserId)));
+    }
+
+    @ApiOperation("搜索，分页，同时查出闲置发布者的信息")
+    @GetMapping("find")
+    public ResultVo findIdleItem(@RequestParam(value = "findValue",required = false) String findValue,
+                                 @RequestParam(value = "page",required = false) Integer page,
+                                 @RequestParam(value = "nums",required = false) Integer nums){
+        if(null==findValue){
+            findValue="";
+        }
+        int p=1;
+        int n=8;
+        if(null!=page){
+            p=page>0?page:1;
+        }
+        if(null!=nums){
+            n=nums>0?nums:8;
+        }
+        return ResultVo.success(idleItemService.findIdleItem(findValue,p,n));
+    }
+
+    @ApiOperation("分类查询，分页，同时查出闲置发布者的信息，代码结构与上面的类似，可封装优化，或改为join查询")
+    @GetMapping("lable")
+    public ResultVo findIdleItemByLable(@RequestParam(value = "idleLabel",required = true) Integer idleLabel,
+                                 @RequestParam(value = "page",required = false) Integer page,
+                                 @RequestParam(value = "nums",required = false) Integer nums){
+        int p=1;
+        int n=8;
+        if(null!=page){
+            p=page>0?page:1;
+        }
+        if(null!=nums){
+            n=nums>0?nums:8;
+        }
+        return ResultVo.success(idleItemService.findIdleItemByLable(idleLabel,p,n));
+    }
+
+    @ApiOperation("更新闲置信息")
+    @PostMapping("update")
+    public ResultVo updateIdleItem(@CookieValue("shUserId")
+                                       @NotNull(message = "登录异常 请重新登录")
+                                       @NotEmpty(message = "登录异常 请重新登录") String shUserId,
+                                   @RequestBody NxxIdleItem nxxIdleItem){
+        nxxIdleItem.setUserId(Long.valueOf(shUserId));
+        if(idleItemService.updateIdleItem(nxxIdleItem)){
+            return ResultVo.success();
+        }
+        return ResultVo.fail(ErrorMsg.SYSTEM_ERROR);
+    }
+}
---
+++ src/main/java/com/net/nxx/controller/OrderController.java
@@ -0,0 +1,99 @@
+package com.net.nxx.controller;
+
+import com.net.nxx.common.exception.ErrorMsg;
+import com.net.nxx.model.NxxOrder;
+import com.net.nxx.service.OrderService;
+import com.net.nxx.utils.IdFactoryUtil;
+import com.net.nxx.utils.OrderTaskHandler;
+import com.net.nxx.vo.ResultVo;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author wt
+ * @create 2021-05-09
+ */
+@Api(tags = "OrderController", description = "订单管理")
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @ApiOperation("添加订单")
+    @PostMapping("/add")
+    public ResultVo addOrder(@CookieValue("shUserId")
+                             @NotNull(message = "登录异常 请重新登录")
+                             @NotEmpty(message = "登录异常 请重新登录") String shUserId,
+                             @RequestBody NxxOrder nxxOrder){
+        if(OrderTaskHandler.orderService==null){
+            OrderTaskHandler.orderService=orderService;
+        }
+
+
+        nxxOrder.setOrderNumber(IdFactoryUtil.getOrderId());
+        nxxOrder.setCreateTime(new Date());
+        nxxOrder.setUserId(Long.valueOf(shUserId));
+        nxxOrder.setOrderStatus((byte) 0);
+        nxxOrder.setPaymentStatus((byte)0);
+        if(orderService.addOrder(nxxOrder)){
+            return ResultVo.success(nxxOrder);
+        }
+        return ResultVo.fail(ErrorMsg.SYSTEM_ERROR);
+    }
+
+
+    @ApiOperation("获取订单信息")
+    @GetMapping("/info")
+    public ResultVo getOrderInfo(@CookieValue("shUserId")
+                                 @NotNull(message = "登录异常 请重新登录")
+                                 @NotEmpty(message = "登录异常 请重新登录") String shUserId,
+                                 @RequestParam Long id){
+        NxxOrder nxxOrder=orderService.getOrder(id);
+        if(nxxOrder.getUserId().equals(Long.valueOf(shUserId))||
+                nxxOrder.getIdleItem().getUserId().equals(Long.valueOf(shUserId))){
+            return ResultVo.success(nxxOrder);
+        }
+        return ResultVo.fail(ErrorMsg.SYSTEM_ERROR);
+    }
+
+    @ApiOperation("更新订单信息")
+    @PostMapping("/update")
+    public ResultVo updateOrder(@CookieValue("shUserId")
+                             @NotNull(message = "登录异常 请重新登录")
+                             @NotEmpty(message = "登录异常 请重新登录") String shUserId,
+                             @RequestBody NxxOrder nxxOrder){
+        if(nxxOrder.getPaymentStatus()!=null&&nxxOrder.getPaymentStatus().equals((byte) 1)){
+            nxxOrder.setPaymentTime(new Date());
+        }
+        if(orderService.updateOrder(nxxOrder)){
+            return ResultVo.success(nxxOrder);
+        }
+        return ResultVo.fail(ErrorMsg.SYSTEM_ERROR);
+    }
+
+    @ApiOperation("我的订单")
+    @GetMapping("/my")
+    public ResultVo getMyOrder(@CookieValue("shUserId")
+                                 @NotNull(message = "登录异常 请重新登录")
+                                 @NotEmpty(message = "登录异常 请重新登录") String shUserId){
+        return ResultVo.success(orderService.getMyOrder(Long.valueOf(shUserId)));
+    }
+
+    @ApiOperation("我卖出的订单")
+    @GetMapping("/my-sold")
+    public ResultVo getMySoldIdle(@CookieValue("shUserId")
+                               @NotNull(message = "登录异常 请重新登录")
+                               @NotEmpty(message = "登录异常 请重新登录") String shUserId){
+        return ResultVo.success(orderService.getMySoldIdle(Long.valueOf(shUserId)));
+    }
+}
